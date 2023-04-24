a6a00694ee8567ffd06d4dee37966c101349a3e9
Ash Holdsworth <ash.holdsworth@sainsburys.co.uk>
2023-04-24 22:59:17 +0100

    Updated ConsumableItem styling

--- front-end/src/Components/common/consumableItem.tsx
+++ front-end/src/Components/common/consumableItem.tsx
@@ -1,5 +1,7 @@
 import "../../css/consumableItem.css";
 import { IConsumable } from "../../@Types/IConsumables";
+import { IHeaderSize } from "../../@Types/IHeaderSize";
+import { Key } from "react";
 
 export const ConsumableItem = ({
   consumable,
@@ -8,15 +10,39 @@ export const ConsumableItem = ({
   consumable: IConsumable;
   index: number;
 }) => {
-  let stars: string = "";
-  for (let i = 0; i < consumable.rating; i++) {
-    stars = stars + "*";
+  
+  let stars: any = [];
+  for (let i: number = 0; i < consumable.rating; i++) {
+    stars.push(i);
+  }
+
+  let headerSize:IHeaderSize = {
+    fontSize: 22
+  };
+
+  console.log(`Name is ${consumable.name.length}`)
+  if (consumable.name.length > 15) {
+    headerSize = {
+      fontSize: 15
+    };
   }
 
   return (
     <div id="consumable-item" key={index}>
-      <h1>{consumable.name}</h1>
-      <p>Rating: {stars}</p>
+      <div className="header">
+        <h2 style={headerSize}>{consumable.name}</h2>
+      </div>
+      <p>{stars.map((star: Key | null | undefined) => (
+        <img
+        src={require("../../Assets/images/star.png")}
+        alt="search"
+        id="star"
+        key={star}
+      />))}
+        </p>
+        <div id="item-image">
+
+        </div>
     </div>
   );
 };
