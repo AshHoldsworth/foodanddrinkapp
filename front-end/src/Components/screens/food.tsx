4bb66fea1d44985cb1a55883de4882e05f6b6245
Ash Holdsworth <ash.holdsworth@sainsburys.co.uk>
2023-04-26 21:00:12 +0100

    refactor: Formatted food.tsx and added IConsumable to consumablesToDisplay

--- front-end/src/Components/screens/food.tsx
+++ front-end/src/Components/screens/food.tsx
@@ -10,34 +10,42 @@ export const Food = () => {
   const [healthyOption, setHealthyOption] = useState<boolean>();
   const apiClient = new ApiClient(Global.context.urlPath);
 
-  const handleHealthyOption = () => {
-    setHealthyOption(!healthyOption);
-  }
-  
+  const handleHealthyOption = () => setHealthyOption(!healthyOption);
+
   apiClient.get("consumables/food").then((response: any) => {
     setConsumablesList(response);
     console.log(healthyOption);
   });
-  let consumablesToDisplay: any = [];
-  
+
+  let consumablesToDisplay: IConsumable[] = [];
+
   if (consumablesList) {
-    JSON.parse(consumablesList.toString()).map(
-      (consumable: IConsumable) => (
-        healthyOption && consumable.isHealthyOption ? consumablesToDisplay.push(consumable) : (
-          !healthyOption ? consumablesToDisplay.push(consumable) : null 
-        )));
-      }
+    JSON.parse(consumablesList.toString()).map((consumable: IConsumable) =>
+      healthyOption && consumable.isHealthyOption
+        ? consumablesToDisplay.push(consumable)
+        : !healthyOption
+        ? consumablesToDisplay.push(consumable)
+        : null
+    );
+  }
 
   return (
     <>
-    <div id="filter-bar" className=".grid">
-      <p>
-        Healthy Options: <input type="checkbox" checked={healthyOption} onChange={handleHealthyOption}/>
-      </p>
-    </div>
+      <div id="filter-bar" className=".grid">
+        <p>
+          Healthy Options:{" "}
+          <input
+            type="checkbox"
+            checked={healthyOption}
+            onChange={handleHealthyOption}
+          />
+        </p>
+      </div>
       <div id="food-list" className="grid">
-        {consumablesList ? consumablesToDisplay.map((consumable: IConsumable, index: number) => ( 
-          <ConsumableItem consumable={consumable} index={index} />)
+        {consumablesToDisplay ? (
+          consumablesToDisplay.map((consumable: IConsumable, index: number) => (
+            <ConsumableItem consumable={consumable} index={index} />
+          ))
         ) : (
           <p>Nothing to display!</p>
         )}
