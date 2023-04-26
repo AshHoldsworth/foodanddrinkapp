7571cb60a874ee0117220451e6ed11668d5fa9a7
Ash Holdsworth <ash.holdsworth@sainsburys.co.uk>
2023-04-26 23:30:02 +0100

    chore: InitialSeed created

---
+++ back-end/foodanddrinkapp-database/Migrations/202304262258_InitialSeedData.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace foodanddrinkapp_database.Migrations
+{
+    [Migration(202304232258)]
+    public class _202304232258_InitialSeedData : Migration
+    {
+        public override void Up()
+        {
+            Execute.Script("../../../Scripts/SeedConsumables.sql");
+            Execute.Script("../../../Scripts/SeedIngredients.sql");
+            Execute.Script("../../../Scripts/SeedConsumableIngredients.sql");
+        }
+
+        public override void Down()
+        {
+            Delete.FromTable("Consumables").AllRows();
+            Delete.FromTable("Ingredients").AllRows();
+            Delete.FromTable("ConsumableIngredients").AllRows();
+        }
+    }
+}
---
+++ back-end/foodanddrinkapp-database/Scripts/SeedConsumableIngredients.sql
@@ -0,0 +1,20 @@
+﻿PRINT 'Seeding [dbo].[ConsumableIngredients]...';
+
+INSERT INTO [dbo].ConsumableIngredients (ConsumableId, IngredientId)
+VALUES
+	(1, 1),
+	(1, 2),
+	(2, 2),
+	(2, 3),
+	(2, 4),
+	(2, 5),
+	(3, 3),
+	(3, 6),
+	(3, 8),
+	(3, 11),
+	(4, 7),
+	(4, 8),
+	(5, 12),
+	(5, 9),
+	(5, 10),
+	(5, 11);
